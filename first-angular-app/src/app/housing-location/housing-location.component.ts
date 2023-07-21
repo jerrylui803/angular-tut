e68ee4a38ed17bdb36cbd69cb929addecf69bf5c
Jerry Lui <jerrylui803@gmail.com>
2023-07-21 16:51:25 -0400

    finish first-angular-app: lesson 11

--- first-angular-app/src/app/housing-location/housing-location.component.ts
+++ first-angular-app/src/app/housing-location/housing-location.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet
+  ],
   // template: `
   //   <p>
   //     housing-location works!
@@ -17,6 +23,7 @@ import { HousingLocation } from '../housinglocation';
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
