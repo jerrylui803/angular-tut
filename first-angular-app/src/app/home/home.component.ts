e5d7f0b03106fd011a9f5d2a1755a37b8f1ab59b
Jerry Lui <jerrylui803@gmail.com>
2023-07-24 10:28:04 -0400

    finish first-angular-app: lesson 12

--- first-angular-app/src/app/home/home.component.ts
+++ first-angular-app/src/app/home/home.component.ts
@@ -12,14 +12,7 @@ import { HousingService } from '../housing.service';
     CommonModule,
     HousingLocationComponent
   ],
-  //   template: `
-  //   <section>
-  //     <form>
-  //       <input type="text" placeholder="Filter by city">
-  //       <button class="primary" type="button">Search</button>
-  //     </form>
-  //   </section>
-  // `,
+
   template: `
   <section>
     <form>
@@ -28,8 +21,7 @@ import { HousingService } from '../housing.service';
     </form>
   </section>
   <section class="results">
-    <!--<app-housing-location></app-housing-location>-->
-    <!--<app-housing-location [housingLocation]="housingLocation"></app-housing-location>-->
+
     <app-housing-location
       *ngFor="let housingLocation of housingLocationList"
       [housingLocation]="housingLocation">
@@ -42,17 +34,6 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-
-  // housingLocation: HousingLocation = {
-  //   id: 9999,
-  //   name: 'Test Home',
-  //   city: 'Test city',
-  //   state: 'ST',
-  //   photo: `${this.baseUrl}/example-house.jpg`,
-  //   availableUnits: 99,
-  //   wifi: true,
-  //   laundry: false,
-  // };
 
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
