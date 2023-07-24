acb5be6b24b59810baa990d813217cefc7a4492f
Jerry Lui <jerrylui803@gmail.com>
2023-07-24 15:55:23 -0400

    finish angular-tour-of-heroes: create a feature component

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+
+  @Input() hero?: Hero;
+
+}
